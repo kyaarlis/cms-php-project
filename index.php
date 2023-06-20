ad7522b8a856f0ed293239b98976b8cb39b575a6
Karlis Veckagans <veckaganskarlis@gmail.com>
2023-06-20 19:23:46 +0300

    dinamically fetching data from db

--- index.php
+++ index.php
@@ -24,21 +24,30 @@
                 $query = "SELECT * FROM posts";
 
                 $post_query = mysqli_query($conn, $query);
+
+                while ($post = mysqli_fetch_assoc($post_query)) {
+                    $title = $post['title'];
+                    $author = $post['author'];
+                    $date = $post['date'];
+                    $img = $post['image'];
+                    $content = $post['content'];
+
+                    echo "
+                    <h2>
+                    <a href='#'>{$title}</a>
+                </h2>
+                <p class='lead'>
+                    by <a href='index.php'>{$author}</a>
+                </p>
+                <p><span class='glyphicon glyphicon-time'></span> Posted on {$date}</p>
+                <hr>
+                <img class='img-responsive' src={$img} alt=''>
+                <hr>
+                <p>{$content}</p>
+                <a class='btn btn-primary' href='#'>Read More <span class='glyphicon glyphicon-chevron-right'></span></a>";
+                }
                 
                 ?>
-
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
 
