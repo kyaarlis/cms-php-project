dd9b0280c6c862326e080d02ea56e08b195d8cd0
Karlis Veckagans <veckaganskarlis@gmail.com>
2023-06-20 19:05:43 +0300

    first commit

---
+++ db/db.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$password = "";
+$admin = "root";
+$db = "cms";
+
+$conn = mysqli_connect($host, $admin, $password, $db);
+
+// if ($conn) {
+//     echo "Connected to DB successfully!";
+// }
+
